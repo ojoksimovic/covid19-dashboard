19bbdddd5e57896f4a674efbe4347b4b5fea062c
ojoksimovic <olivera2391@gmail.com>
2023-09-03 19:46:57 -0400

    avoid card duplication by adding condition to componentdidmount function

--- src/ContinentCards.js
+++ src/ContinentCards.js
@@ -1,23 +1,20 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { withStyles } from "@material-ui/core/styles";
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Typography from '@material-ui/core/Typography';
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import React from "react";
 
 const useStyles = makeStyles({
   root: {
     // maxWidth: 800,
-    marginBottom: "10px"},
+    marginBottom: "10px",
+  },
   media: {
     height: 5,
   },
 });
-
 
 let labelContinents = [];
 let casesContinents = [];
@@ -27,100 +24,151 @@ let deathsPerCapContinents = [];
 let fatalityRateContinents = [];
 let continentCards = [];
 
-let cardColor= [
-  'rgb(255, 99, 132)',
-  'rgb(255, 159, 64)',
-  'rgb(255, 205, 86)',
-  'rgb(75, 192, 192)',
-  'rgb(54, 162, 235)',
-  'rgb(153, 102, 255)'
-]
+let cardColor = [
+  "rgb(255, 99, 132)",
+  "rgb(255, 159, 64)",
+  "rgb(255, 205, 86)",
+  "rgb(75, 192, 192)",
+  "rgb(54, 162, 235)",
+  "rgb(153, 102, 255)",
+];
 
 class ContinentCards extends React.Component {
- 
-
-
   testRun() {
-
-    for (let i = 0; i < (this.props.casesContinents).length; i++) {
+    for (let i = 0; i < this.props.casesContinents.length; i++) {
       labelContinents.push(this.props.casesContinents[i]["continent"]);
       casesContinents.push(this.props.casesContinents[i]["cases"]);
       deathsContinents.push(this.props.casesContinents[i]["deaths"]);
-      casesPerCapContinents.push(this.props.casesContinents[i]["casesPerOneMillion"]);
-      deathsPerCapContinents.push(this.props.casesContinents[i]["deathsPerOneMillion"]);
-      fatalityRateContinents.push((this.props.casesContinents[i]["deaths"])/(this.props.casesContinents[i]["cases"]));
+      casesPerCapContinents.push(
+        this.props.casesContinents[i]["casesPerOneMillion"]
+      );
+      deathsPerCapContinents.push(
+        this.props.casesContinents[i]["deathsPerOneMillion"]
+      );
+      fatalityRateContinents.push(
+        this.props.casesContinents[i]["deaths"] /
+          this.props.casesContinents[i]["cases"]
+      );
       this.createCards();
       this.forceUpdate();
     }
   }
 
   componentDidMount() {
-
-    this.testRun();
-  };
+    if (labelContinents.length == 0) {
+      this.testRun();
+    }
+  }
 
   createCards() {
     const { classes } = this.props;
     for (let i = 0; i < labelContinents.length; i++) {
       continentCards[i] = (
-        <div className = "col-xs-8 offset-xs-2 col-sm-6 col-lg-3">
-    <Card className={classes.root} style = {{marginBottom: 10, borderTop: cardColor[i] + " 5px solid"}}>
-        <CardMedia
-          className={classes.media}
-          image = './Coronavirus_3D_illustration_by_CDC_1600x900.png'
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h6" component="h2">
-            {labelContinents[i]}
-          </Typography>
-          <Typography gutterBottom variant="h6" component="h2" style = {{marginBottom: 0}}>
-            {casesContinents[i].toLocaleString()} cases
-          </Typography>
-          <Typography variant="body1" component="p" color = "textSecondary" style={{marginTop: 0, fontSize: "0.8rem", fontStyle: "italic"}}>
-          {casesPerCapContinents[i].toLocaleString()} cases per million
-          </Typography>
-          <Typography variant="h6" component="h2" color = "secondary" style={{marginBottom: 0}}>
-          {deathsContinents[i].toLocaleString()} deaths
-          </Typography>
-          <Typography variant="body1" component="p" color = "textSecondary" style={{marginTop: 0, fontSize: "0.8rem", fontStyle: "italic"}}>
-          {deathsPerCapContinents[i].toLocaleString()} deaths per million
-          </Typography>
-          <Typography variant="body1" color="textSecondary" component="p" style={{marginTop: 5}}>
-            {(fatalityRateContinents[i].toLocaleString(undefined,{style: 'percent', minimumFractionDigits:2}))} Fatality Rate
-          </Typography>
-        </CardContent>
-    </Card>
-    </div>
-
-      )
+        <div className="col-xs-8 offset-xs-2 col-sm-6 col-lg-3">
+          <Card
+            className={classes.root}
+            style={{ marginBottom: 10, borderTop: cardColor[i] + " 5px solid" }}
+          >
+            <CardMedia
+              className={classes.media}
+              image="./Coronavirus_3D_illustration_by_CDC_1600x900.png"
+              title="Contemplative Reptile"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h6" component="h2">
+                {labelContinents[i]}
+              </Typography>
+              <Typography
+                gutterBottom
+                variant="h6"
+                component="h2"
+                style={{ marginBottom: 0 }}
+              >
+                {casesContinents[i].toLocaleString()} cases
+              </Typography>
+              <Typography
+                variant="body1"
+                component="p"
+                color="textSecondary"
+                style={{
+                  marginTop: 0,
+                  fontSize: "0.8rem",
+                  fontStyle: "italic",
+                }}
+              >
+                {casesPerCapContinents[i].toLocaleString()} cases per million
+              </Typography>
+              <Typography
+                variant="h6"
+                component="h2"
+                color="secondary"
+                style={{ marginBottom: 0 }}
+              >
+                {deathsContinents[i].toLocaleString()} deaths
+              </Typography>
+              <Typography
+                variant="body1"
+                component="p"
+                color="textSecondary"
+                style={{
+                  marginTop: 0,
+                  fontSize: "0.8rem",
+                  fontStyle: "italic",
+                }}
+              >
+                {deathsPerCapContinents[i].toLocaleString()} deaths per million
+              </Typography>
+              <Typography
+                variant="body1"
+                color="textSecondary"
+                component="p"
+                style={{ marginTop: 5 }}
+              >
+                {fatalityRateContinents[i].toLocaleString(undefined, {
+                  style: "percent",
+                  minimumFractionDigits: 2,
+                })}{" "}
+                Fatality Rate
+              </Typography>
+            </CardContent>
+          </Card>
+        </div>
+      );
     }
     this.forceUpdate();
   }
 
   render() {
-
     if (labelContinents.length === 0) {
       return (
         <div class="text-center">
-          <div className="spinner-border" style={{ width: "3rem", height: "3rem" }} role="status">
+          <div
+            className="spinner-border"
+            style={{ width: "3rem", height: "3rem" }}
+            role="status"
+          >
             <span className="visually-hidden"></span>
           </div>
           <h3 id="loading-text">Loading...this may take up to 60 seconds...</h3>
         </div>
-      )
+      );
     }
     for (let i = 0; i < labelContinents.length; i++) {
       return (
-        <div> 
-          <h1 class="display-4 text-center" id = "continent-text" style = {{fontSize: "3rem", margin: 25}}>Covid-19 by Continent</h1>
-          <hr/>
-        <div className = "row">
-          {continentCards} 
-    </div>
-    <hr/>
-    </div>
-
-      )
-    }}}
-    export default withStyles(useStyles)(ContinentCards);
+        <div>
+          <h1
+            class="display-4 text-center"
+            id="continent-text"
+            style={{ fontSize: "3rem", margin: 25 }}
+          >
+            Covid-19 by Continent
+          </h1>
+          <hr />
+          <div className="row">{continentCards}</div>
+          <hr />
+        </div>
+      );
+    }
+  }
+}
+export default withStyles(useStyles)(ContinentCards);
--- src/TopCountryCard.js
+++ src/TopCountryCard.js
@@ -1,27 +1,22 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { withStyles } from "@material-ui/core/styles";
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-
+import React from "react";
+import { Link } from "react-router-dom";
 
 const useStyles = makeStyles({
   root: {
     // maxWidth: 800,
-    marginBottom: "10px"},
+    marginBottom: "10px",
+  },
   media: {
     height: 5,
   },
 });
-
 
 let labelCountry = [];
 let casesCountry = [];
@@ -31,97 +26,159 @@ let percentWorldCases = [];
 let fatalityRateCountry = [];
 let countryCards = [];
 
-
 class TopCountryCard extends React.Component {
- 
-
-
   testRun() {
-
     for (let i = 0; i < 4; i++) {
       labelCountry.push(this.props.casesCountries[i]["country"]);
       casesCountry.push(this.props.casesCountries[i]["cases"]);
-      casesPercentagePop.push((this.props.casesCountries[i]["cases"])/(this.props.casesCountries[i]["population"]));
-      todayCases.push(this.props.casesCountries[i]["todayCases"]);  
-      percentWorldCases.push((this.props.casesCountries[i]["cases"])/(this.props.cases["cases"]));
-      fatalityRateCountry.push((this.props.casesCountries[i]["deaths"])/(this.props.casesCountries[i]["cases"]));
+      casesPercentagePop.push(
+        this.props.casesCountries[i]["cases"] /
+          this.props.casesCountries[i]["population"]
+      );
+      todayCases.push(this.props.casesCountries[i]["todayCases"]);
+      percentWorldCases.push(
+        this.props.casesCountries[i]["cases"] / this.props.cases["cases"]
+      );
+      fatalityRateCountry.push(
+        this.props.casesCountries[i]["deaths"] /
+          this.props.casesCountries[i]["cases"]
+      );
       this.createCards();
       this.forceUpdate();
     }
   }
 
   componentDidMount() {
-
-    this.testRun();
-  };
+    if (labelCountry.length == 0) {
+      this.testRun();
+    }
+  }
 
   createCards() {
     const { classes } = this.props;
     for (let i = 0; i < labelCountry.length; i++) {
       countryCards[i] = (
-        <div className = "col-xs-8 offset-xs-2 col-sm-6 col-lg-3">
-    <Card className={classes.root} style = {{marginBottom: 10, borderTop: "rgb(255, 99, 132) 5px solid"}}>
-      <CardActionArea>
-        <Link style = {{textDecoration:"none", color:"black"}}to={"/country/"+labelCountry[i]}>
-        <CardMedia
-          className={classes.media}
-        />
+        <div className="col-xs-8 offset-xs-2 col-sm-6 col-lg-3">
+          <Card
+            className={classes.root}
+            style={{
+              marginBottom: 10,
+              borderTop: "rgb(255, 99, 132) 5px solid",
+            }}
+          >
+            <CardActionArea>
+              <Link
+                style={{ textDecoration: "none", color: "black" }}
+                to={"/country/" + labelCountry[i]}
+              >
+                <CardMedia className={classes.media} />
 
-        <CardContent>
-          <Typography gutterBottom variant="h6" component="h2">
-            {labelCountry[i]}
-          </Typography>
-          <Typography gutterBottom variant="h6" component="h2" style = {{display: "inline", marginBottom: 0}}>
-            {casesCountry[i].toLocaleString()} cases
-          </Typography>
-          <Typography variant="body1" component="p" color = "textSecondary" style={{display: "inline", marginTop: 0, marginLeft:5, fontSize: "0.8rem"}}>
-          {todayCases[i]? "+" : null}{todayCases[i]? todayCases[i].toLocaleString():null}
-          </Typography>
-          <Typography variant="h6" component="h3" style={{fontWeight:"bold", marginBottom: 5, fontSize: "1rem"}}>
-          {percentWorldCases[i].toLocaleString(undefined,{style: 'percent', minimumFractionDigits:2})} of total world cases
-          </Typography>
-          <Typography variant="body1" component="p" color = "textSecondary" style={{marginTop: 0, fontSize: "0.8rem"}}>
-          {casesPercentagePop[i].toLocaleString(undefined,{style: 'percent', minimumFractionDigits:2})} of country infected
-          </Typography>
-          <Typography variant="body1" component="p" color = "textSecondary" style={{marginTop: 0, fontSize: "0.8rem"}}>
-            {(fatalityRateCountry[i].toLocaleString(undefined,{style: 'percent', minimumFractionDigits:2}))} fatality rate
-          </Typography>
-        </CardContent>
-        </Link>
-      </CardActionArea>
-    </Card>
-    </div>
-
-      )
+                <CardContent>
+                  <Typography gutterBottom variant="h6" component="h2">
+                    {labelCountry[i]}
+                  </Typography>
+                  <Typography
+                    gutterBottom
+                    variant="h6"
+                    component="h2"
+                    style={{ display: "inline", marginBottom: 0 }}
+                  >
+                    {casesCountry[i].toLocaleString()} cases
+                  </Typography>
+                  <Typography
+                    variant="body1"
+                    component="p"
+                    color="textSecondary"
+                    style={{
+                      display: "inline",
+                      marginTop: 0,
+                      marginLeft: 5,
+                      fontSize: "0.8rem",
+                    }}
+                  >
+                    {todayCases[i] ? "+" : null}
+                    {todayCases[i] ? todayCases[i].toLocaleString() : null}
+                  </Typography>
+                  <Typography
+                    variant="h6"
+                    component="h3"
+                    style={{
+                      fontWeight: "bold",
+                      marginBottom: 5,
+                      fontSize: "1rem",
+                    }}
+                  >
+                    {percentWorldCases[i].toLocaleString(undefined, {
+                      style: "percent",
+                      minimumFractionDigits: 2,
+                    })}{" "}
+                    of total world cases
+                  </Typography>
+                  <Typography
+                    variant="body1"
+                    component="p"
+                    color="textSecondary"
+                    style={{ marginTop: 0, fontSize: "0.8rem" }}
+                  >
+                    {casesPercentagePop[i].toLocaleString(undefined, {
+                      style: "percent",
+                      minimumFractionDigits: 2,
+                    })}{" "}
+                    of country infected
+                  </Typography>
+                  <Typography
+                    variant="body1"
+                    component="p"
+                    color="textSecondary"
+                    style={{ marginTop: 0, fontSize: "0.8rem" }}
+                  >
+                    {fatalityRateCountry[i].toLocaleString(undefined, {
+                      style: "percent",
+                      minimumFractionDigits: 2,
+                    })}{" "}
+                    fatality rate
+                  </Typography>
+                </CardContent>
+              </Link>
+            </CardActionArea>
+          </Card>
+        </div>
+      );
     }
     this.forceUpdate();
   }
 
   render() {
-
     if (labelCountry.length == 0) {
       return (
         <div class="text-center">
-          <div className="spinner-border" style={{ width: "3rem", height: "3rem" }} role="status">
+          <div
+            className="spinner-border"
+            style={{ width: "3rem", height: "3rem" }}
+            role="status"
+          >
             <span className="visually-hidden"></span>
           </div>
           <h3 id="loading-text">Loading...this may take up to 60 seconds...</h3>
         </div>
-      )
+      );
     }
     for (let i = 0; i < labelCountry.length; i++) {
       return (
-        <div> 
-          <h1 class="display-4 text-center" id = "country-text" style = {{fontSize: "3rem", margin: 25}}>
+        <div>
+          <h1
+            class="display-4 text-center"
+            id="country-text"
+            style={{ fontSize: "3rem", margin: 25 }}
+          >
             Most Affected Countries
-            </h1>
-          <hr/>
-        <div className = "row">
-          {countryCards}
-    </div>
-    <hr/>
-    </div>
-
-      )
-    }}}
-    export default withStyles(useStyles)(TopCountryCard);
+          </h1>
+          <hr />
+          <div className="row">{countryCards}</div>
+          <hr />
+        </div>
+      );
+    }
+  }
+}
+export default withStyles(useStyles)(TopCountryCard);
